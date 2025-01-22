ba06e79d00f5ba2ed9d9b6339c037fc071056add
Vadim <vadshustow@ya.ru>
2025-01-23 01:53:34 +0700

    KS-l1-t1 rename variables

--- src/main/kotlin/lesson1/KS-l1-t1.kt
+++ src/main/kotlin/lesson1/KS-l1-t1.kt
@@ -2,6 +2,6 @@ package org.example.lesson1
 
 fun main() {
 
-    val ordersQuantity: Int = 75
-    val purchaseGratitude: String = "Спасибо за покупку!"
+    val numberOfOrders: Int = 75
+    val gratitudeMessage: String = "Спасибо за покупку!"
 }
