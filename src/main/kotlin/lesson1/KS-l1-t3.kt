55847eb28666eb5628d8f027d74cb5cfb0ee59d5
Vadim <vadshustow@ya.ru>
2025-01-23 20:18:50 +0700

    KS-l1-t3 create new variables. Print all variables

---
+++ src/main/kotlin/lesson1/KS-l1-t3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+
+    val year: Short = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+}
