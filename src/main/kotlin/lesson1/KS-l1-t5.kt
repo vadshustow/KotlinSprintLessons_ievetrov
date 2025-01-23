876ec745e317dd8f5f80bd19f626d8940e53a55d
Vadim <vadshustow@ya.ru>
2025-01-23 23:23:31 +0700

    KS-l1-t5 solved the task.

---
+++ src/main/kotlin/lesson1/KS-l1-t5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main() {
+
+    var seconds = 6480
+
+    val hour = seconds / 3600
+    val minute = (seconds - (hour * 3600)) / 60
+    seconds = (seconds - (hour * 3600)) - (minute * 60)
+
+    println("${"%02d".format(hour)}:${"%02d".format(minute)}:${"%02d".format(seconds)}")
+}
+
+//    println("%02d".format(hour))
