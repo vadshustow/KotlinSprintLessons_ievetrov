94ac17231fbcd3fa9e3ccecde070e819da568e78
Vadim <vadshustow@ya.ru>
2025-01-25 22:29:23 +0700

    KS-2-3 created a constant.

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,14 +1,16 @@
 package org.example.lesson2
 
+const val SIXTY = 60
+
 fun main() {
 
     val departureHours = 9
     val departureMinutes = 39
     val minutesOnTheRoad = 457
 
-    val totalMinutes = ((departureHours * 60) + departureMinutes) + minutesOnTheRoad
-    val arrivalHours = totalMinutes / 60
-    val arrivalMinutes = totalMinutes - (arrivalHours * 60)
+    val totalMinutes = ((departureHours * SIXTY) + departureMinutes) + minutesOnTheRoad
+    val arrivalHours = totalMinutes / SIXTY
+    val arrivalMinutes = totalMinutes - (arrivalHours * SIXTY)
 
     println("Время прибытия поезда: $arrivalHours:$arrivalMinutes")
 }
