c8bfb0555fffdbcc134eb4bae1cc908f91750442
Vadim <vadshustow@ya.ru>
2025-01-27 01:47:35 +0700

    KS-3-2 implemented printing of full name and age.

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $patronymic, $age")
+}
