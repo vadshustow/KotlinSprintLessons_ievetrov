0ead4f5cb16d334edf86d5afba9a9ae7d60c4021
Vadim <vadshustow@ya.ru>
2025-01-27 01:29:54 +0700

    KS-3-1 created and print strings variables.

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson3
+
+fun main() {
+
+    val userName = "Вадим"
+    var message = "Добрый день"
+
+    println("$message, $userName")
+
+    message = "Добрый вечер"
+
+    println("$message, $userName")
+}
