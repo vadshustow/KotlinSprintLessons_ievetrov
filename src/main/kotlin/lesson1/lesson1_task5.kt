f4e0c672537b36f7f50329f3112b8e5bef0d5b48
Vadim <vadshustow@ya.ru>
2025-01-26 23:29:50 +0700

    KS-1-5 added constants

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,12 +1,15 @@
 package org.example.lesson1
 
+const val SIXTY = 60
+const val THREE_THOUSAND_SIX_HUNDRED = 3600
+
 fun main() {
 
     var seconds = 6480
 
-    val hour = seconds / 3600
-    val minute = (seconds - (hour * 3600)) / 60
-    seconds = (seconds - (hour * 3600)) - (minute * 60)
+    val hour = seconds / THREE_THOUSAND_SIX_HUNDRED
+    val minute = (seconds - (hour * THREE_THOUSAND_SIX_HUNDRED)) / SIXTY
+    seconds = (seconds - (hour * THREE_THOUSAND_SIX_HUNDRED)) - (minute * SIXTY)
 
     println("${"%02d".format(hour)}:${"%02d".format(minute)}:${"%02d".format(seconds)}")
 }
